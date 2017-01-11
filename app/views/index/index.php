157cc05e48fa6ad1b6e937e55cfd2c941f24307f
nilosweden <info@fsx.se>
2017-01-11 09:57:56 +0100

    Update index.php

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EPG Web</title>
+    <title>FSX Framework</title>
     <link href="/mvc/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
